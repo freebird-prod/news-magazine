fdf9583ec3121537011723e9f663e0909814da4e
Dharun Kumar  S H <freebirdprod.official@gmail.com>
2025-06-20 08:42:33 +0530

    k

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -4,7 +4,7 @@ const Navbar = ({setCategory}) => {
     return (
         <nav className="navbar navbar-expand-lg bg-body-tertiary" data-bs-theme="dark">
             <div className="container-fluid">
-                <a className="navbar-brand" href="#"><span className='badge bg-light text-dark fs-5'>News Mag</span></a>
+                <a className="navbar-brand" href="#"><span className='badge bg-light text-dark fs-5'>Buzz Byte</span></a>
                 <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                     <span className="navbar-toggler-icon"></span>
                 </button>
